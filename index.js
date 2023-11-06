7d229e6984cc1681ef4e969a685e6c43e1371560
neelimapriya <neelimasultana6@gmail.com>
2023-11-06 13:27:33 +0600

    setup cors middleware

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 
 const secret = "secrettoken";
 // middleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173'
+  ],
+  credentials:true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
